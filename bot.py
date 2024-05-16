302380e07cf3ea840d50c8cc125cf78702bfa309
Mhmdali Abbasbeygi <57040347+mhmdaliab@users.noreply.github.com>
2024-05-17 01:49:56 +0330

    Update bot.py os.getenv

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@
 import os
 import telebot
 
-API_KEY = 7008948652:AAFoDBxL8KqeqM_tD_u1MPCy5Lb05wLymB8 #os.getenv ('TELEGRAM_TOKEN')
+API_KEY = os.getenv ('TELEGRAM_TOKEN')
 
 @bot.messege_handler(commands=['Greet'])
 def Greet(message):
