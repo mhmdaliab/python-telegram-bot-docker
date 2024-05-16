6f67142e042945bdc883baeb3f530bf679fd02b5
Mhmdali Abbasbeygi <57040347+mhmdaliab@users.noreply.github.com>
2024-05-17 02:09:25 +0330

    Update bot.py telebot

--- bot.py
+++ bot.py
@@ -5,8 +5,9 @@ import os
 import telebot
 
 API_KEY = os.getenv ('TELEGRAM_TOKEN')
+bot = telebot.TeleBot(API_KEY)
 
-bot.messege_handler(commands=['Greet'])
+@bot.messege_handler(commands=['Greet'])
 def Greet(message):
   bot.replay_to(message, "Hey! How R U?")
 
